879af9b0a8a4e6d47ce89ebdff33a77bd0271f65
tclindner <tclindner@users.noreply.github.com>
2019-07-13 12:27:56 -0500

    Update prettier

--- index.js
+++ index.js
@@ -30,16 +30,19 @@ const config = {
     'scripts-type': 'error',
     'version-type': 'error',
     'valid-values-author': ['error', ['Thomas Lindner']],
-    'valid-values-engines': ['error', [
-      {
-        node: '>=6.0.0',
-        npm: '>=3.0.0'
-      },
-      {
-        node: '>=8.0.0',
-        npm: '>=6.0.0'
-      }
-    ]],
+    'valid-values-engines': [
+      'error',
+      [
+        {
+          node: '>=6.0.0',
+          npm: '>=3.0.0'
+        },
+        {
+          node: '>=8.0.0',
+          npm: '>=6.0.0'
+        }
+      ]
+    ],
     'valid-values-private': ['error', [false]],
     'prefer-no-version-zero-dependencies': 'warning',
     'prefer-caret-version-dependencies': 'error',
